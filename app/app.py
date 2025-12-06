7a03b9f4063f3385c41c1d1ef7c57a7654f21418
PratikSutar97 <pratiksutar1901@gmail.com>
2025-12-06 23:23:41 +0530

    python code updated

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return jsonify(message="Hello from Dockerized Flask!")
+    return jsonify(message="Hello from Dockerized Flask updated!")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
